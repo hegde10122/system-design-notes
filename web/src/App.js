90fda1d07cc18689b7bec3ed70b001e75ac8a61a
hegde10122 <hegde421201@gmail.com>
2025-09-15 00:55:45 +0530

    changed app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch("/notes/01-introduction.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>System Design Notes</h1>
+      <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
 }
